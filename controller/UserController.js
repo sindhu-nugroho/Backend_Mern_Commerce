ca9b09141773059c761c9f1cd4f5909d0710714d
GrassEater2 <tiantirta@gmail.com>
2025-06-20 15:40:06 +0700

    Finishing Controller folder/section by programming Transaction COntroller Function + Adding UserController function

---
+++ controller/UserController.js
@@ -0,0 +1,22 @@
+const User = require('../models/User');
+
+// Mendapatkan list semua pengguna/user
+exports.getUsers = async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+// Menambahkan pengguna/user baru
+exports.createUser = async (req, res) => {
+    try {
+        const user = new User(req.body);
+        await user.save();
+        res.status(201).json(user);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+}
